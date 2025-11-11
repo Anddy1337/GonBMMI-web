f27502290e585e9274599230d61296d6af80b3a3
Anddy1337 <qxk76zpk9x@privaterelay.appleid.com>
2025-11-11 02:21:56 +0100

    Add shared constants

---
+++ shared.js
@@ -0,0 +1,46 @@
+// Shared constants and helpers for YT Smart Skip (MV3)
+(() => {
+  const DEFAULT_CATEGORIES = [
+    'sponsor',
+    'selfpromo',
+    'interaction',
+    'intro',
+    'outro',
+    'music_offtopic'
+  ];
+
+  const CATEGORY_LABELS = {
+    sponsor: 'Sponsor',
+    selfpromo: 'Self-promotion',
+    interaction: 'Interaction reminders (Like/Subscribe)',
+    intro: 'Intro',
+    outro: 'Outro',
+    music_offtopic: 'Non-music (in music videos)'
+  };
+
+  const CATEGORY_COLORS = {
+    sponsor: '#ffd700',
+    selfpromo: '#ffa500',
+    interaction: '#ff69b4',
+    intro: '#1e90ff',
+    outro: '#8a2be2',
+    music_offtopic: '#00ced1'
+  };
+
+  const STORAGE_KEYS = {
+    categories: 'categories',
+    autoSkipEnabled: 'autoSkipEnabled',
+    sponsorColor: 'sponsorColor',
+    categoryColors: 'categoryColors'
+  };
+
+  const SB_API = 'https://sponsor.ajay.app/api/skipSegments';
+
+  window.YTSmartSkip = Object.freeze({
+    DEFAULT_CATEGORIES,
+    CATEGORY_LABELS,
+    CATEGORY_COLORS,
+    STORAGE_KEYS,
+    SB_API
+  });
+})();
